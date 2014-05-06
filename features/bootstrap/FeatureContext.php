c266ef7efe3ea553f41842e60152ffd75e1a58d8
salvoscala <salvoscala@gmail.com>
2014-05-06 11:52:01 +0200

    Cleaning code

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -24,6 +24,7 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
   /**
     *
     * @var     string
+    * Unit and Booking Parametres
   */
   public $unit_id;
   public $booking_id;
@@ -32,6 +33,7 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
   public $unit_name = "test";
   public $unit_max_sleeps = 2;
   public $order_id;
+  public $customer = "test_customer";
   /**
   * Initializes context.
   * Every scenario gets its own context object.
@@ -42,7 +44,7 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
     // Initialize your context here
   }   
   /**
-  * Click some text
+  * Click some text in the current page
   *
   * @When /^I click on the text "([^"]*)"$/
   */
@@ -66,52 +68,48 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
    */
   public function iClickOnTheElementWithXPath($xpath) {
       
-    $session = $this->getSession(); // get the mink session
+    $session = $this->getSession();
     $element = $session->getPage()->find(
         'xpath',
         $session->getSelectorsHandler()->selectorToXpath('xpath', $xpath)
-    ); // runs the actual query and returns the element
-
-    // errors must not pass silently
+    );
     if (null === $element) {
         throw new \InvalidArgumentException(sprintf('Could not evaluate XPath: "%s"', $xpath));
     }
-    // let's click on it
     $element->click();
   }    
   /**
-  * Add Bookable unit
+  * Add BookableUnitType
   *
-  * @Given /^I add Bookable Unit named "(?P<name>(?:[^"]|\\")*)" with price "(?P<price>(?:[^"]|\\")*)"$/
+  * @Given /^I add BookableUnitType "(?P<name>(?:[^"]|\\")*)" with price "(?P<price>(?:[^"]|\\")*)"$/
   */
-  public function addBookableUnit($name, $price) {
+  public function addBookableUnitType($name, $price) {
       
     $this->assertAtPath("/admin/rooms/units/unit-types/add");
     $this->fillField("label", $name);
     $this->fillField("data[base_price]", $price);
-    $this->fillField("edit-max-sleeps", $this->unit_max_sleeps);
-    $this->pressButton("op");
+    $this->fillField("edit-data-max-sleeps", $this->unit_max_sleeps);
+    $this->pressButton("edit-submit");
   }
   /**
-  * Check Bookable Unit Type
+  * Looking for BookableUnitType, if it is not present we will create it.
   *
   * @Given /^I have BookableUnitType "(?P<name>(?:[^"]|\\")*)"$/
   */
   public function checkBookableUnitType($name) {
-      
     try {
-        $this->assertAtPath("/admin/rooms/units/unit-types");
-        $this->assertSession()->elementContains('xpath','//TABLE[@CLASS="sticky-enabled tableheader-processed sticky-table"]',$name);
+      $this->assertAtPath("/admin/rooms/units/unit-types");
+      $this->assertSession()->elementContains('xpath','//TABLE[@CLASS="sticky-enabled tableheader-processed sticky-table"]',$name);
     } catch (Exception $rte) {
-        $this->addBookableUnit($name,$this->test_price);
-    }
+        $this->addBookableUnit($name,$this->unit_price);
+      }
   }
   /**
-  * Check Bookable Unit
+  * Check Bookable Unit, if it is not present we will create it.
   *
   * @Given /^I have BookableUnit "(?P<name>(?:[^"]|\\")*)" of type "(?P<type>(?:[^"]|\\")*)" with default state "(?P<state>(?:[^"]|\\")*)"$/
   */
-  public function checkBookableUnit($name,$type,$state) {
+  public function checkBookableUnit($name, $type, $state) {
           
     $this->assertAtPath("/admin/rooms/units");
     try {        
@@ -121,9 +119,9 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
         $this->iClickOnTheText("Add ".$type." unit");
         $this->fillField("edit-name", $name);
         $this->fillField("edit-default-state",$state);
-        $this->pressButton("op");
-    }
-        $this->checkUnitState($name, $state);
+        $this->pressButton("edit-submit");
+      }
+    $this->checkUnitState($name, $state);
   }    
   /**
   * Attempts to check the state of a Unit
@@ -131,7 +129,6 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
   * @Given /^the state of unit "(?P<unit>[^"]*)" is "(?P<state>[^"]*)"$/
   */
   public function checkUnitState($unit, $state) {
-    
     $this->assertAtPath("/admin/rooms/units");
     $this->assertClickInTableRow("Edit", $unit);
     $page = $this->getSession()->getCurrentUrl();
@@ -142,21 +139,22 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
     $this->pressButton("edit-submit");
   }  
   /**
-  * add bookingEvent
+  * Update availability or price ("type") of the unit Test.
+  * To run this function i need to ChecUnitState before.
   *
   * @When /^I update "(?P<type>[^"]*)" to state "(?P<state>[^"]*)" from "(?P<start>[^"]*)" to "(?P<end>[^"]*)"$/
   */
   public function updateBookingEvent($type, $state, $start, $end) {
-
+    
     $this->assertAtPath("/admin/rooms/units");
     if ($type == "availability") {
         $this->assertClickInTableRow("Manage Availability", $this->unit_id);
-        sleep(5);
-        $this->iClickOnTheText("Update Availability");
+        sleep(3);
+        $this->iClickOnTheText("Update Unit Availability");
     }
     if ($type == "price") {
         $this->assertClickInTableRow("Manage Pricing", $this->unit_id);
-        sleep(5);
+        sleep(3);
         $this->iClickOnTheText("Update Pricing");
     }
     $this->fillField("rooms_start_date[date]", $start);
@@ -169,10 +167,9 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
     $this->pressButton("edit-submit");
   }
   /**
-  * Return the last day of month (28/29/30/31)
+  * Return number of days in a month
   */
   public function endOfMonth($month, $year) {
-      
     if ($month == "01" || $month == "1" || $month == 1)
         return 31;
     if ($month == "02" || $month == "2"|| $month == 2) {
@@ -204,18 +201,19 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
         return 31;
   }
   /**
-  * Check Room state
+  * Check Json Event of unit. Type can be availability or price.
+  * To run this function i need to CheckUnitState before.
   *
   * @Then /^the "(?P<type>[^"]*)" state from "(?P<start>[^"]*)" to "(?P<end>[^"]*)" should be "(?P<state>[^"]*)"$/
   */
   public function checkJsonEvent($type, $start, $end, $state) {
       
-    //Taking start day, start month e start year
+    //Taking start day, start month, start year
     $tmp = explode("/", $start);
     $day_start = $tmp[0];
     $month_start = $tmp[1];
     $year_start = $tmp[2];
-    //Taking end day, end month e end year
+    //Taking end day, end month, end year
     $tmp = explode("/", $end);
     $day_end = $tmp[0];
     $month_end = $tmp[1];
@@ -227,6 +225,8 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
         $state = "AV";
     if ($state == "2")
         $state = "ON-REQ";
+    if ($state == "3")
+        $state = $this->customer;
     if ($type == "availability")
         $url = "/?q=rooms/units/unit/".$this->unit_id."/availability/json/";
     if ($type == "price")
@@ -234,7 +234,7 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
     $this->assertAtPath($url.$year_start."/".$month_start."/".$day_start."/".$year_end."/".$month_end."/".$day_end);
     sleep(5);
     $year_count = (int)$year_end - (int)$year_start;
-    //number of Months
+    //Number of Months
     if ($year_count == 0)
       $months = (int)$month_end - (int)$month_start;
     else {
@@ -293,51 +293,50 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
     }
   }
    /**
-  * Add a test_customer
+  * Add a customer. 
   *
   * @Given /^I have a customer "(?P<name>[^"]*)"$/
   */
-  public function addTestCostumer($name) {
+  public function addCostumer($name) {
     $this->assertAtPath("/admin/commerce/customer-profiles");
     try {
       $this->assertSession()->elementContains('xpath','//TABLE[@CLASS="views-table cols-6"]',$name);
     } catch (Exception $e) {
         $this->iClickOnTheText("Add a customer profile");
         $this->fillField("edit-commerce-customer-address-und-0-name-line", $name);
-        $this->fillField("edit-commerce-customer-address-und-0-thoroughfare", "Via Roma 123");
-        $this->fillField("edit-commerce-customer-address-und-0-postal-code", "12345");
-        $this->fillField("edit-commerce-customer-address-und-0-locality", "Ragusa");
+        $this->fillField("edit-commerce-customer-address-und-0-thoroughfare", "Via Sicilia 72");
+        $this->fillField("edit-commerce-customer-address-und-0-postal-code", "97015");
+        $this->fillField("edit-commerce-customer-address-und-0-locality", "Ispica");
         $this->fillField("edit-commerce-customer-address-und-0-administrative-area", "RG");
         $this->pressButton("edit-submit");
       }
     
   }
  /**
-  * Add a booking for unit Test
+  * Add a booking for unit Test 
   *
   * @Given /^I have a booking from "(?P<start>[^"]*)" to "(?P<end>[^"]*)"$/
   */
   public function addBooking($start,$end) {
     $price = $this->unit_price * $this->daysBetweenDates($start, $end);
     $this->assertAtPath("/admin/rooms/units");
-    $this->unit_id =$this->findElementInATable($this->unit_type_name, 0);
-    $this->assertAtPath("/admin/rooms");
-    $this->iClickOnTheText("Bookings");
-    sleep(4);
+    $this->unit_id = $this->findElementInATable($this->unit_type_name, 0);
+    $this->assertAtPath("/admin/rooms/bookings");
+    sleep(3);
     $this->iClickOnTheText("Add a Booking");
-    $this->fillField("edit-client", "test_customer");
-    sleep(4);
+    $this->fillField("edit-client", $this->customer);
+    sleep(3);
     $this->fillField("rooms_start_date[date]", $start);
     $this->fillField("rooms_end_date[date]", $end);
     $this->pressButton("edit-get-availability");
-    sleep(4);
-    $this->fillField("edit-unit-type", "test");
-    sleep(4);
+    sleep(3);
+    $this->fillField("edit-unit-type", $this->unit_name);
+    sleep(3);
     $this->assertSelectRadioById("Test - Cost: $ ".$price,"edit-unit-id-".$this->unit_id);
-    sleep(4);
+    sleep(3);
     $this->checkOption("edit-booking-status");
     $this->pressButton("edit-submit");
-    $this->booking_id = $this->findElementInATable("test_customer", 0);
+    $this->booking_id = $this->findElementInATable($this->customer, 0);
   }
   /**
   * Calculate the number of days between two dates
@@ -377,7 +376,7 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
     //Taking Order ID
     $tmp = explode("/", $page);
     $this->order_id = $tmp[5];
-    $this->fillField("edit-customer-profile-billing-commerce-customer-address-und-0-name-line", "test_customer");
+    $this->fillField("edit-customer-profile-billing-commerce-customer-address-und-0-name-line", $this->customer);
     $this->fillField("edit-customer-profile-billing-commerce-customer-address-und-0-thoroughfare", "Via Roma 123");
     $this->fillField("edit-customer-profile-billing-commerce-customer-address-und-0-postal-code", "123456");
     $this->fillField("edit-customer-profile-billing-commerce-customer-address-und-0-locality", "Ragusa");
@@ -445,8 +444,8 @@ class FeatureContext extends Drupal\DrupalExtension\Context\DrupalContext {
         if ($value == $id) {
          $element = $tmp[$position];
         }
-    if ($element != "null")
-      break;
+      if ($element != "null")
+        break;
     }
     return $element;
   }
